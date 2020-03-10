c3d5450b6f2d14aa1e27bfdff3c654b020741397
tombetthauser <tombetthauser@gmail.com>
2020-03-10 00:47:20 -0700

    Links all working

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const LINKS = [
+  "introduction",
+  "demographics",
+  "connections",
+  "conclusions"
+]
+
 document.addEventListener('DOMContentLoaded', (event) => {
   let style = document.querySelector(".mobile-drop-down").style;
   document.querySelector(".main-header-nav").addEventListener("click", () => {
@@ -13,4 +20,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
       style.height = "0px";
     }
   });
+
+  const linkShow = (link) => {
+    for (let i = 0; i < LINKS.length; i++) {
+      document.querySelector(`.${LINKS[i]}-div`).style.display = "none";
+    }
+    document.querySelector(`.${link}-div`).style.display = "block";
+  }
+
+  for (let i = 0; i < LINKS.length; i++) {
+    let links = document.querySelectorAll(`.${LINKS[i]}-link`);
+    for (let j = 0; j < links.length; j++) {
+      document.querySelectorAll(`.${LINKS[i]}-link`)[j].addEventListener("click", () => {
+        linkShow(`${LINKS[i]}`)
+      })
+    }
+  }
 })
