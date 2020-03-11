cf192b12a9ea986132ee5eb57e68035a151ae736
tombetthauser <tombetthauser@gmail.com>
2020-03-11 16:19:50 -0700

    Add keywords data

--- src/render.js
+++ src/render.js
@@ -1,8 +1,16 @@
-import { FOUND_SURVEY, RACE, GENDER, AGE, ORIENTATION } from "./data";
+import { FOUND_SURVEY, RACE, GENDER, AGE, ORIENTATION, KEYWORDS } from "./data";
 import BarGraph from "./BarGraph";
 import PieChart from "./PieChart";
 
 export const RENDER_GRAPHS = () => {
+  new BarGraph({
+    title: "What words describe your work?",
+    tag: ".graph-keywords",
+    data: KEYWORDS,
+    color: "#E0BBE4",
+    width: 8000,
+  }).render();
+
   new BarGraph({
     title: "What is your gender?",
     tag: ".graph-gender",
@@ -23,7 +31,7 @@ export const RENDER_GRAPHS = () => {
     title: "What is your race / ethnicity?",
     tag: ".graph-race",
     data: RACE,
-    color: "#ddd",
+    color: "#8da290",
     width: 400,
     angle: true,
   }).render();
