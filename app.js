a4494e961c59300a9a154c70d92badda7c3d26cc
tombetthauser <tombetthauser@gmail.com>
2020-03-10 17:51:37 -0700

    Get graph class basics functioning

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+import Graph from "./source/graph";
+
+const t = new Graph;
+
+t.test();
+
 const LINKS = [
   "introduction",
   "demographics",
@@ -7,7 +13,7 @@ const LINKS = [
 const COLOR = "lightgray";
 
 const linkShow = (link) => {
-  for (i = 0; i < LINKS.length; i++) {
+  for (let i = 0; i < LINKS.length; i++) {
     document.querySelector(`.${LINKS[i]}-div`).style.display = "none";
   }
   document.querySelector(`.${link}-div`).style.display = "block";
@@ -46,6 +52,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
 
+  t.barGraph();
+
+
   // D3 code starts here for first bar graph.
 
   let data = [
@@ -63,7 +72,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
   let xAxis = g => g.attr("transform", `translate(0,${height - margin.bottom})`).call(d3.axisBottom(x));
   let yAxis = g => g.attr("transform", `translate(${margin.left},0)`).call(d3.axisLeft(y).tickFormat(d3.format(".0%")));
   let svg = d3.select('.graph-gender');
-  let g = svg.append("g").attr("fill", COLOR);
+  let g = svg.append("g");
 
   g.selectAll("rect").data(data).join("rect").attr("x", d => x(d.name)).attr("y", d => y(d.value)).attr("height", d => y(0) - y(d.value)).attr("width", x.bandwidth());
   svg.append("g").call(xAxis);
@@ -93,7 +102,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
   x = d3.scaleBand().domain(data.map(d => d.name)).range([margin.left, width - margin.right]).padding(0.1);
   y = d3.scaleLinear().domain([0, d3.max(data, d => d.value)]).range([height - margin.bottom, margin.top]);
   svg = d3.select('.graph-race');
-  g = svg.append("g").attr("fill", COLOR);
+  g = svg.append("g");
   g.selectAll("rect").data(data).join("rect").attr("x", d => x(d.name)).attr("y", d => y(d.value)).attr("height", d => y(0) - y(d.value)).attr("width", x.bandwidth());
   svg.append("g").call(xAxis);
   svg.append("g").call(yAxis);
@@ -114,7 +123,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
   x = d3.scaleBand().domain(data.map(d => d.name)).range([margin.left, width - margin.right]).padding(0.1);
   y = d3.scaleLinear().domain([0, d3.max(data, d => d.value)]).range([height - margin.bottom, margin.top]);
   svg = d3.select('.graph-orientation');
-  g = svg.append("g").attr("fill", COLOR);
+  g = svg.append("g");
   g.selectAll("rect").data(data).join("rect").attr("x", d => x(d.name)).attr("y", d => y(d.value)).attr("height", d => y(0) - y(d.value)).attr("width", x.bandwidth());
   svg.append("g").call(xAxis);
   svg.append("g").call(yAxis);
@@ -136,7 +145,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
   x = d3.scaleBand().domain(data.map(d => d.name)).range([margin.left, width - margin.right]).padding(0.1);
   y = d3.scaleLinear().domain([0, d3.max(data, d => d.value)]).range([height - margin.bottom, margin.top]);
   svg = d3.select('.graph-age');
-  g = svg.append("g").attr("fill", COLOR);
+  g = svg.append("g");
   g.selectAll("rect").data(data).join("rect").attr("x", d => x(d.name)).attr("y", d => y(d.value)).attr("height", d => y(0) - y(d.value)).attr("width", x.bandwidth());
   svg.append("g").call(xAxis);
   svg.append("g").call(yAxis);
@@ -145,18 +154,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
   
   // D3 how did you find this graph code is below.
 
-  data = [
-    { name: "artist / peer", value: "0.198" },
-    { name: "instagram", value: "0.733" },
-    { name: "other", value: "0.069" },
-  ];
+  // data = [
+  //   { name: "artist / peer", value: "0.198" },
+  //   { name: "instagram", value: "0.733" },
+  //   { name: "other", value: "0.069" },
+  // ];
 
-  x = d3.scaleBand().domain(data.map(d => d.name)).range([margin.left, width - margin.right]).padding(0.1);
-  y = d3.scaleLinear().domain([0, d3.max(data, d => d.value)]).range([height - margin.bottom, margin.top]);
-  svg = d3.select('.graph-found-survey');
-  g = svg.append("g").attr("fill", COLOR);
-  g.selectAll("rect").data(data).join("rect").attr("x", d => x(d.name)).attr("y", d => y(d.value)).attr("height", d => y(0) - y(d.value)).attr("width", x.bandwidth());
-  svg.append("g").call(xAxis);
-  svg.append("g").call(yAxis);
+  // x = d3.scaleBand().domain(data.map(d => d.name)).range([margin.left, width - margin.right]).padding(0.1);
+  // y = d3.scaleLinear().domain([0, d3.max(data, d => d.value)]).range([height - margin.bottom, margin.top]);
+  // svg = d3.select('.graph-found-survey');
+  // g = svg.append("g");
+  // g.selectAll("rect").data(data).join("rect").attr("x", d => x(d.name)).attr("y", d => y(d.value)).attr("height", d => y(0) - y(d.value)).attr("width", x.bandwidth());
+  // svg.append("g").call(xAxis);
+  // svg.append("g").call(yAxis);
 
 })
