e4bed01a24f954fcca3067ed867dc9d68406ddca
tombetthauser <tombetthauser@gmail.com>
2020-03-14 00:50:42 -0700

    Get map cleaned up

--- states.js
+++ states.js
@@ -1,3 +1,27 @@
 jQuery(document).ready(function () {
-	jQuery('#vmap').vectorMap({ map: 'usa_en' });
+	jQuery('#vmap').vectorMap(
+		{
+			map: 'usa_en',
+			backgroundColor: '#fff',
+			borderColor: '#fff',
+			borderOpacity: 0.25,
+			borderWidth: 3.5,
+			color: 'rgba(255,0,0,.1)',
+			enableZoom: false,
+			// hoverColor: 'rgba(255,0,0,.5)',
+			hoverOpacity: 0.5,
+			normalizeFunction: 'linear',
+			scaleColors: ['red', 'blue'],
+			selectedColor: 'rgba(255,0,0,.5)',
+			selectedRegions: null,
+			showTooltip: false,
+			// onRegionClick: function (element, code, region) {
+			// 	var message = 'You clicked "'
+			// 		+ region
+			// 		+ '" which has the code: '
+			// 		+ code.toUpperCase();
+
+			// 	alert(message);
+			// }
+		});
 });
