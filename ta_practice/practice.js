b8037aa677dfb4bb8ad88d5fdea4b0e2c73d2449
tombetthauser <tombetthauser@gmail.com>
2020-04-14 05:27:02 -0700

    Add tests for rec_sum

--- ta_practice/practice.js
+++ ta_practice/practice.js
@@ -55,12 +55,56 @@ const caesar_cipher = (string, num) => {
 // from another string.
 
 // Bonus: solve it in O(m * n) using O(m * n) extra space. (Hint: the solution involves 
-//   dynamic programming which will be introduced later in the course.)
+// dynamic programming which will be introduced later in the course.)
+
+const longest_common_substring = (string1, string2) => {
+  // get substrings arrays of both strings
+  // sort them by length and reverse
+  // iterate over one set
+  // return when a match is found
+
+  let subs1 = substrings(string1);
+  let subs2 = substrings(string2);
+
+  let longest = '';
+
+  subs1.forEach(sub => {
+    if (subs2.includes(sub) && sub.length > longest.length) {
+      longest = sub;
+    }
+  })
+
+  return longest;
+}
+
+const substrings = string => {
+  let subs = [];
+
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + 1; j < string.length + 1; j++) {
+      let sub = string.slice(i, j);
+      subs.push(sub);
+    }
+  }
+
+  return subs;
+}
+
+// console.log(substrings('cat'))
+
+
+// console.log(longest_common_substring('abc', 'bce')) // => 'bc'
+// console.log(longest_common_substring('catdogmouse', 'catmousefrog')) // => 'mouse'
+// console.log(longest_common_substring('12345', '52341')) // => '234'
 
 
 // sum_rec
 
 // Write a function that takes an array of integers and returns their sum.Use recursion.
+
+console.log(sum_rec([3])) // => 3
+console.log(sum_rec([1,2,3])) // => 6
+console.log(sum_rec([1,1,3,4,5])) // => 14
 
 
 
